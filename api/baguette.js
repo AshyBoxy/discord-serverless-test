c5adb50dd2b004dd5ab3d9aee7180ea47cc5f81f
AshyBoxy <ashyboxy@ashyboxy.xyz>
2023-05-12 16:29:53 +0100

    ew

--- api/baguette.js
+++ api/baguette.js
@@ -1,6 +1,30 @@
+import * as nacl from "tweetnacl";
+
 export default function handler(req, res) {
-    console.log(req.body);
-    return res.json({
-        foo: "bar"
-    });
+    if (req.method === 'POST') {
+        const buf = await buffer(req);
+        const rawBody = buf.toString('utf8');
+
+        console.log(rawBody);
+
+        // Can do something here...
+        res.json({ rawBody });
+    } else {
+        res.setHeader('Allow', 'POST');
+        res.status(405).end('Method Not Allowed');
+    }
 }
+
+async function buffer(readable: Readable) {
+    const chunks = [];
+    for await (const chunk of readable) {
+        chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
+    }
+    return Buffer.concat(chunks);
+}
+
+export const config = {
+    api: {
+        bodyParser: false,
+    }
+};
