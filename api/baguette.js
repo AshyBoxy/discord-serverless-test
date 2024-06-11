89cf365c2fc090cdb0cb75b46ca10980f91da1f7
AshyBoxy <ashyboxy@ashyboxy.xyz>
2024-06-11 19:39:23 +0100

    add norwegian

--- api/baguette.js
+++ api/baguette.js
@@ -13,7 +13,8 @@ const defaultMessageResponse = {
 const baguetteResponses = {
     "french": "Je ne suis qu'une baguette de pain, que voulez-vous que je fasse ?",
     "italian": "Sono solo una baguette, cosa ti aspetti che faccia?",
-    "polish": "Jestem tylko bagietką, czego ode mnie oczekujesz?"
+    "polish": "Jestem tylko bagietką, czego ode mnie oczekujesz?",
+    "norwegian": "Jeg er bare en baguette, hva forventer du at jeg skal gjøre?"
 }
 
 /**
